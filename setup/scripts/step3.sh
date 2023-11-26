ca0dbfd01fec43e131c9b4e984b1fafefe44680f
Andrew Pettey <andrew@pettey.me>
2023-11-26 14:41:35 +0100

    fix: step3

--- setup/scripts/step3.sh
+++ setup/scripts/step3.sh
@@ -3,5 +3,5 @@
 # Install microk8s
 snap install microk8s --classic
 mkdir -p ~/.kube
-usermod -a -G microk8s $USER
-chown -R $USER ~/.kube
+sudo usermod -a -G microk8s $USER
+sudo chown -R $USER ~/.kube
